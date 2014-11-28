8386686844364b190b4365946997cb09c4e25b2d
Ole Aamot <oka@oka.no>
2014-11-28 23:16:29 +0100

    Add license boilerplate and fix space

--- src/girl-listener.h
+++ src/girl-listener.h
@@ -1,4 +1,25 @@
-/* $Id: girl-station.h,v 1.3 2002/10/04 20:20:30 oka Exp $ */
+/* $Id$
+ *
+ * GNOME Internet Radio Locator
+ *
+ * Copyright (C) 2014  Ole Aamot Software
+ *
+ * Author: Ole Aamot <oka@oka.no>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
 
 #ifndef GIRL_LISTENER_H
 #define GIRL_LISTENER_H
